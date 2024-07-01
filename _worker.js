217dc55d3b1a7b82284672c3872383c4df389360
zhy121212 <142400711+zhy121212@users.noreply.github.com>
2024-07-01 23:57:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -6,7 +6,6 @@ let mytoken= ['auto'];//快速订阅访问入口, 留空则不启动快速订阅
 // 设置优选地址，不带端口号默认443，TLS订阅生成
 let addresses = [
 	'icook.tw:2053#官方优选域名',
-	'cloudflare.cfgo.cc#优选官方线路',
 ];
 
 // 设置优选地址api接口
@@ -18,8 +17,6 @@ let addressesapi = [
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
 let addressesnotls = [
 	'www.visa.com.sg#官方优选域名',
-	'www.wto.org:8080#官方优选域名',
-	'www.who.int:8880#官方优选域名',
 ];
 
 // 设置优选noTLS地址api接口
